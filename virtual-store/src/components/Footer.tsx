229ee7709ef4ff59910365374f9688b216d36b2d
iribel97 <irinaochoa32@gmail.com>
2024-06-06 17:25:21 -0500

    Actividad - Implementar las clases de Tailwind en Footer. FINALIZADA

--- virtual-store/src/components/Footer.tsx
+++ virtual-store/src/components/Footer.tsx
@@ -1,99 +1,100 @@
-import styles from "./Footer.module.css";
+//import styles from "./Footer.module.css";
 
 function Footer() {
   return (
     <>
-    <footer>
-    <div id="footer" className={styles.footerContainer}>
-        <ul className={styles.footerUl}>
-          <li className={styles.footerMainItem}>
-            <a className={styles.footerA} href="./index.html">
-              Ofertas
-            </a>
-          </li>
-          <li className={styles.footerLi}>
-            <a className={styles.footerA} href="./index.html">
-              Laptops
-            </a>
-          </li>
-          <li className={styles.footerLi}>
-            <a className={styles.footerA} href="./index.html">
-              Audio
-            </a>
-          </li>
-          <li className={styles.footerLi}>
-            <a className={styles.footerA} href="./index.html">
-              Auriculares
-            </a>
-          </li>
-        </ul>
-        <ul className={styles.footerUl}>
-          <li className={styles.footerMainItem}>
-            <a className={styles.footerA} href="./index.html">
-              Cómo comprar
-            </a>
-          </li>
-          <li className={styles.footerLi}>
-            <a className={styles.footerA} href="./index.html">
-              Formas de pago
-            </a>
-          </li>
-          <li className={styles.footerLi}>
-            <a className={styles.footerA} href="./index.html">
-              Envios
-            </a>
-          </li>
-          <li className={styles.footerLi}>
-            <a className={styles.footerA} href="./index.html">
-              Devoluciones
-            </a>
-          </li>
-        </ul>
-        <ul className={styles.footerUl}>
-          <li className={styles.footerMainItem}>
-            <a className={styles.footerA} href="./index.html">
-              Costos y tarifas
-            </a>
-          </li>
-          <li className={styles.footerLi}>
-            <a className={styles.footerA} href="./index.html">
-              Impuestos
-            </a>
-          </li>
-          <li className={styles.footerLi}>
-            <a className={styles.footerA} href="./index.html">
-              Facturación
-            </a>
-          </li>
-        </ul>
-        <ul className={styles.footerUl}>
-          <li className={styles.footerMainItem}>
-            <a className={styles.footerA} href="./index.html">
-              Mis pedidos
-            </a>
-          </li>
-          <li className={styles.footerLi}>
-            <a className={styles.footerA} href="./index.html">
-              Pedir nuevamente
-            </a>
-          </li>
-          <li className={styles.footerLi}>
-            <a className={styles.footerA} href="./index.html">
-              Lista de deseos
-            </a>
-          </li>
-        </ul>
-        <ul className={styles.footerUl}>
-          <li className={styles.footerMainItem}>
-            <a className={styles.footerA} href="./index.html">
-              Garantía
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div className={styles.footerCopyright}>Curso de HTML + CSS 2023</div>
-    </footer>
-      
+    <footer className="w-full h-min-[250px] bg-[#7525a7] flex flex-col items-center">
+        <div id="footer" className="w-[1080px] flex justify-between p-[10px]">
+          <ul className="w-[170px] list-none p-[10px]">
+            <li className="font-bold pb-[20px]">
+              <a className="text-white no-underline" href="./index.html">
+                Ofertas
+              </a>
+            </li>
+            <li className="py-[5px] px-0">
+              <a className="text-white no-underline" href="./index.html">
+                Laptops
+              </a>
+            </li>
+            <li className="py-[5px] px-0">
+              <a className="text-white no-underline" href="./index.html">
+                Audio
+              </a>
+            </li>
+            <li className="py-[5px] px-0">
+              <a className="text-white no-underline" href="./index.html">
+                Auriculares
+              </a>
+            </li>
+          </ul>
+          <ul className="w-[170px] list-none p-[10px]">
+            <li className="font-bold pb-[20px]">
+              <a className="text-white no-underline" href="./index.html">
+                Cómo comprar
+              </a>
+            </li>
+            <li className="py-[5px] px-0">
+              <a className="text-white no-underline" href="./index.html">
+                Formas de pago
+              </a>
+            </li>
+            <li className="py-[5px] px-0">
+              <a className="text-white no-underline" href="./index.html">
+                Envios
+              </a>
+            </li>
+            <li className="py-[5px] px-0">
+              <a className="text-white no-underline" href="./index.html">
+                Devoluciones
+              </a>
+            </li>
+          </ul>
+          <ul className="w-[170px] list-none p-[10px]">
+            <li className="font-bold pb-[20px]">
+              <a className="text-white no-underline" href="./index.html">
+                Costos y tarifas
+              </a>
+            </li>
+            <li className="py-[5px] px-0">
+              <a className="text-white no-underline" href="./index.html">
+                Impuestos
+              </a>
+            </li>
+            <li className="py-[5px] px-0">
+              <a className="text-white no-underline" href="./index.html">
+                Facturación
+              </a>
+            </li>
+          </ul>
+          <ul className="w-[170px] list-none p-[10px]">
+            <li className="font-bold pb-[20px]">
+              <a className="text-white no-underline" href="./index.html">
+                Mis pedidos
+              </a>
+            </li>
+            <li className="py-[5px] px-0">
+              <a className="text-white no-underline" href="./index.html">
+                Pedir nuevamente
+              </a>
+            </li>
+            <li className="py-[5px] px-0">
+              <a className="text-white no-underline" href="./index.html">
+                Lista de deseos
+              </a>
+            </li>
+          </ul>
+          <ul className="w-[170px] list-none p-[10px]">
+            <li className="font-bold pb-[20px]">
+              <a className="text-white no-underline" href="./index.html">
+                Garantía
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="w-[1080px] text-center text-white font-bold py-[30px] px-0 border-t-[1px] border-t-[#ba53fb]">
+          Curso de HTML + CSS 2023
+        </div>
+      </footer>
     </>
   );
 }
