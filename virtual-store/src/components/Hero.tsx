52194dc5342dddd228586db3c505e91c38050e11
iribel97 <irinaochoa32@gmail.com>
2024-06-06 17:04:53 -0500

    Actividad - Implementar las clases de Tailwind en Hero. FINALIZADA

--- virtual-store/src/components/Hero.tsx
+++ virtual-store/src/components/Hero.tsx
@@ -1,11 +1,11 @@
-import styles from "./Hero.module.css";
+//import styles from "./Hero.module.css";
 
 function Hero({firstText, secondText}) {
   return (
-    <section className={styles["hero-section"]}>
-      <article className={styles["hero-title"]}>
-        <span className={styles["hero-span"]}>{firstText}</span>
-        <span className={styles["hero-span"]}>{secondText}</span>
+    <section className="h-[200px] bg-[url('../public/hero_bg.png')] bg-cover bg-[center_top] flex justify-center">
+      <article className="w-[1080px] flex flex-col items-start justify-center pl-20">
+        <span className="text-[40px] text-white font-bold">{firstText}</span>
+        <span className="text-[80px] leading-[30px] text-white font-bold">{secondText}</span>
       </article>
     </section>
   );
